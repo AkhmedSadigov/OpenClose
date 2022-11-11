27e366275a2e59239c9892740758490f201ac75f
ahmed <ahmed@LAPTOP-D0D6I3G9>
2022-11-11 16:04:36 +0400

    Add project files.

---
+++ OpenClose/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace OpenClose
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnectDisconnect = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnectDisconnect
+            // 
+            this.buttonConnectDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnectDisconnect.Location = new System.Drawing.Point(297, 259);
+            this.buttonConnectDisconnect.Name = "buttonConnectDisconnect";
+            this.buttonConnectDisconnect.Size = new System.Drawing.Size(181, 71);
+            this.buttonConnectDisconnect.TabIndex = 0;
+            this.buttonConnectDisconnect.Text = "Connect";
+            this.buttonConnectDisconnect.UseVisualStyleBackColor = true;
+            this.buttonConnectDisconnect.Click += new System.EventHandler(this.buttonConnectDisconnect_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(176, 86);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(160, 29);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Disconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonConnectDisconnect);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnectDisconnect;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ OpenClose/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OpenClose
+{
+
+    public partial class Form1 : Form
+    {
+    private SqlConnection conn = new SqlConnection("Server=LAPTOP-D0D6I3G9\\SQLEXPRESS; Database=Northwind; Integrated Security=true;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnectDisconnect_Click(object sender, EventArgs e)
+        {
+            if(conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+                buttonConnectDisconnect.Text = "Connect";
+                labelStatus.Text = "Disconnected";
+            }
+            else
+            {
+                conn.Open();
+                buttonConnectDisconnect.Text = "Disconnect";
+                labelStatus.Text = "Connected";
+            }
+        }
+    }
+}
